5c6aa60322fa55c48628d0681782734045c24d9d
bastianverarivera <b.vera05@ufromail.cl>
2022-10-24 15:39:40 -0300

    subo hospital finalizado

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -8,7 +8,7 @@ public class Launcher {
 		Hospital hospital = new Hospital("Hospital Quepe");
 		Paciente paciente1= new Paciente("No es jugador de LOL",37,"Masculino","Cristiano Ronaldo", 20106947-5);
 		Médico doctor1 = new Médico("Título presencial", 29,"Masculino", "Alejandro Fernandez");
-		Médico doctor2 = new Médico("Título online", 34,"Femenino", "Silvia Muñoz");
+		Médico doctor2 = new Médico("Título online", 34,"Femenino", "Silvia Muñoz Neira");
 		hospital.agregarPaciente(paciente1);
 		hospital.contratarMédico(doctor1);
 		hospital.despedirMédico(doctor2);
