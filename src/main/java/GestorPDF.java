8532c5bc748d7b5adbc74fd9d600585d127da234
bastianverarivera <b.vera05@ufromail.cl>
2022-11-07 15:06:31 -0300

    subo primer avance hospital lunes 7 de octubre

---
+++ src/main/java/GestorPDF.java
@@ -0,0 +1,9 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import java.io.IOException;
+import java.util.Date;
+public class GestorPDF {
+}
